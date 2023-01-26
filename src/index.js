87afd9773e4d2c37924293ddc7c3af14beb24014
Angori4 <pavlichuk0810@gmail.com>
2023-01-27 01:47:17 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -1,176 +1,99 @@
+import { pixabayAPI } from "./js/fetch"
+import axios from "axios";
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-import SearchService from './search-service';
 
-import './css/styles.css';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+const btnSearch = document.querySelector(".search-form-button");
+const input = document.querySelector(".search-form-input");
+const gallery = document.querySelector(".gallery");
+const btnLoadMore = document.querySelector(".load-more");
 
-let useInfiniteScroll = false;
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
-const searchService = new SearchService();
+let page = 0;
+let perPage = 40;
+let inputValue = input.value;
 
-const refs = {
-  formSearch:    document.querySelector('#search-form'),
-  gallery:       document.querySelector('.gallery'),
-  btnSearch:     document.querySelector('button[type="submit"]'),
-  btnLoadMore:   document.querySelector('button.load-more'),
-  finishText:    document.querySelector('.finish-text'),
-  guardDiv:      document.querySelector('.js-guard'),
-  infiniteCheck: document.querySelector('.infinite-scroll-check'),
-};
 
-if (Object.values(refs).some(el => !el)) {
-  throw new Error('Error: invalid markup!');
-}
+btnSearch.addEventListener('click', onSearch);
+btnLoadMore.addEventListener('click', onLoadMore);
 
-const observerOpts = {
-  root: null,
-  rootMargin: '300px',
-  threshold: 1.0
-}
-
-const onObserve = (entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      onLoadMore();
-    }
-  });
-}
-
-const observer = new IntersectionObserver(onObserve, observerOpts);
-
-const lightboxOpts = {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-};
-
-const lightbox = new SimpleLightbox('.gallery .gallery__link', lightboxOpts);
-
-refs.formSearch.addEventListener('submit', onSearch);
-refs.btnLoadMore.addEventListener('click', onLoadMore);
-
-function onSearch(evt) {
-  evt.preventDefault();
-
-  const searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
-  
-  if (!searchQuery) {
-    Notiflix.Notify.failure('Please enter a search query!');
-    return;
+  function onSearch(event) {
+   event.preventDefault()
+   inputValue = input.value.trim();
+  if (!inputValue) {
+    gallery.innerHTML = ""; 
+    return
   }
+    page = 1;
+    btnLoadMore.hidden = true;
+  pixabayAPI(inputValue, page, perPage)
+    .then(data => {
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.');
+        gallery.innerHTML = "";
+        btnLoadMore.hidden = true;
+        return;
+      } else {
+        gallery.innerHTML = "";
+        markupGallery(data.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${data.totalHits} images.`
+        );
+        gallerySimpleLightbox.refresh();
+        btnLoadMore.hidden = false;
+        
+        const { height: cardHeight } = document.
+          querySelector(".gallery").
+          firstElementChild.getBoundingClientRect();
 
-  useInfiniteScroll = refs.infiniteCheck.checked;
-  refs.infiniteCheck.disabled = true;
-
-  initQuery(searchQuery);
-  performQuery();
+         window.scrollBy({
+         top: cardHeight * -1,
+         behavior: "smooth",
+         });
+      }
+    }).catch(err => console.log(err));
+    
 }
 
 function onLoadMore() {
-  searchService.incrementPage();
-  performQuery();
-}
-
-function initQuery(searchQuery) {
-  searchService.setNewQuery(searchQuery);
-  refs.gallery.innerHTML = '';
-  if (useInfiniteScroll) {
-    observer.unobserve(refs.guardDiv);
-  } else {
-    refs.btnLoadMore.classList.add('is-hidden');
-  }
-  refs.finishText.classList.add('is-hidden');
-}
-
-async function performQuery() {
-  try {
-    refs.btnSearch.disabled = true;
-    refs.btnLoadMore.disabled = true;
-
-    const data = await searchService.getNextData();
-
-    if (!data || (data.length == 0)) {
-      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      return;
-    }
-      
-    if (searchService.page === 1) {
-      Notiflix.Notify.success(`Hooray! We found ${searchService.resultsQty} images.`);
-      if (useInfiniteScroll) {
-        observer.observe(refs.guardDiv);
-      }
-    }
-
-    refs.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(data));
-
-    refreshPage();
-
-  } catch (err) {
-    console.error(err);
-  } finally {
-    refs.btnSearch.disabled   = false;
-    refs.btnLoadMore.disabled = false;
-  }
-}
-
-function createGalleryMarkup(data) {
-  return data.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${largeImageURL}">
-        <div class="gallery__thumb">
-          <img 
-            class="gallery__image"
-            src="${webformatURL}"
-            data-source="${largeImageURL}"
-            alt="${tags}"
-            loading="lazy" />
-        </div>    
-        <div class="gallery__info">
-          <p class="info__item">
-            <b>Likes</b><br>${likes}
-          </p>
-          <p class="info__item">
-            <b>Views</b><br>${views}
-          </p>
-          <p class="info__item">
-            <b>Comments</b><br>${comments}
-          </p>
-          <p class="info__item">
-            <b>Downloads</b><br>${downloads}
-          </p>
-        </div>
-      </a>
-    </li>
-  `).join('');
-}
-
-function refreshPage() {
-
-  lightbox.refresh();
-
-  if (searchService.isLastPage()) {
-    if (useInfiniteScroll) {
-      observer.unobserve(refs.guardDiv);
-    } else {
-      refs.btnLoadMore.classList.add('is-hidden');
-    }
-    refs.finishText.classList.remove('is-hidden');
-  } else {
-    if (!useInfiniteScroll) {
-      refs.btnLoadMore.classList.remove('is-hidden');
-    }
-    refs.finishText.classList.add('is-hidden');
-  }
+  page += 1
+  inputValue = input.value.trim();
  
-  if ((searchService.page > 1) && !useInfiniteScroll) {   
+  pixabayAPI(inputValue, page, perPage).then(data => {
+    markupGallery(data.hits);
+    let totalPages = data.totalHits / perPage;
+    gallerySimpleLightbox.refresh();
+    
+    if (page >= totalPages) {
+      btnLoadMore.hidden = true;
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.") 
+    }
+  })
+    .catch(err => console.log(err))
+}
 
-    const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: "smooth",
-    });
-  }
+function markupGallery(images) {
+  const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
+  `<div class="photo-card">
+  <a href="${largeImageURL}"><img class="photo"  src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info_item">
+      <b>Likes</b><span class="info_item-api">${likes}</span>
+    </p>
+    <p class="info_item">
+      <b>Views</b><span class="info_item-api">${views}</span>
+    </p>
+    <p class="info_item">
+      <b>Comments</b><span class="info_item-api">${comments}</span>
+    </p>
+    <p class="info_item">
+      <b>Downloads</b><span class="info_item-api">${downloads}</span>
+    </p>
+  </div>
+</div>`).join('');
+  gallery.insertAdjacentHTML("beforeend",markup);
 }

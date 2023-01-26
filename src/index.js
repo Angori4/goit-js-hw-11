70dd8bd5df36ac4b33f11e5585f4c86550a83cb7
Angori4 <pavlichuk0810@gmail.com>
2023-01-27 01:24:13 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -1,87 +1,176 @@
-import './index.html'
-import './css/styles.css'
-import { fetchImages } from './js/fetch-images'
-import { renderGallery } from './js/render-gallery'
-import { onScroll, onToTopBtn } from './js/scroll'
-import Notiflix from 'notiflix'
-import SimpleLightbox from 'simplelightbox'
-import 'simplelightbox/dist/simple-lightbox.min.css'
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 
-const searchForm = document.querySelector('#search-form')
-const gallery = document.querySelector('.gallery')
-const loadMoreBtn = document.querySelector('.btn-load-more')
-let query = ''
-let page = 1
-let simpleLightBox
-const perPage = 40
+import SearchService from './search-service';
 
-searchForm.addEventListener('submit', onSearchForm)
-loadMoreBtn.addEventListener('click', onLoadMoreBtn)
+import './css/styles.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-onScroll()
-onToTopBtn()
+let useInfiniteScroll = false;
 
-function onSearchForm(e) {
-  e.preventDefault()
-  window.scrollTo({ top: 0 })
-  page = 1
-  query = e.currentTarget.searchQuery.value.trim()
-  gallery.innerHTML = ''
-  loadMoreBtn.classList.add('is-hidden')
+const searchService = new SearchService();
 
-  if (query === '') {
-    alertNoEmptySearch()
-    return
+const refs = {
+  formSearch:    document.querySelector('#search-form'),
+  gallery:       document.querySelector('.gallery'),
+  btnSearch:     document.querySelector('button[type="submit"]'),
+  btnLoadMore:   document.querySelector('button.load-more'),
+  finishText:    document.querySelector('.finish-text'),
+  guardDiv:      document.querySelector('.js-guard'),
+  infiniteCheck: document.querySelector('.infinite-scroll-check'),
+};
+
+if (Object.values(refs).some(el => !el)) {
+  throw new Error('Error: invalid markup!');
+}
+
+const observerOpts = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0
+}
+
+const onObserve = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      onLoadMore();
+    }
+  });
+}
+
+const observer = new IntersectionObserver(onObserve, observerOpts);
+
+const lightboxOpts = {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox('.gallery .gallery__link', lightboxOpts);
+
+refs.formSearch.addEventListener('submit', onSearch);
+refs.btnLoadMore.addEventListener('click', onLoadMore);
+
+function onSearch(evt) {
+  evt.preventDefault();
+
+  const searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
+  
+  if (!searchQuery) {
+    Notiflix.Notify.failure('Please enter a search query!');
+    return;
   }
 
-  fetchImages(query, page, perPage)
-    .then(({ data }) => {
-      if (data.totalHits === 0) {
-        alertNoImagesFound()
-      } else {
-        renderGallery(data.hits)
-        simpleLightBox = new SimpleLightbox('.gallery a').refresh()
-        alertImagesFound(data)
+  useInfiniteScroll = refs.infiniteCheck.checked;
+  refs.infiniteCheck.disabled = true;
 
-        if (data.totalHits > perPage) {
-          loadMoreBtn.classList.remove('is-hidden')
-        }
+  initQuery(searchQuery);
+  performQuery();
+}
+
+function onLoadMore() {
+  searchService.incrementPage();
+  performQuery();
+}
+
+function initQuery(searchQuery) {
+  searchService.setNewQuery(searchQuery);
+  refs.gallery.innerHTML = '';
+  if (useInfiniteScroll) {
+    observer.unobserve(refs.guardDiv);
+  } else {
+    refs.btnLoadMore.classList.add('is-hidden');
+  }
+  refs.finishText.classList.add('is-hidden');
+}
+
+async function performQuery() {
+  try {
+    refs.btnSearch.disabled = true;
+    refs.btnLoadMore.disabled = true;
+
+    const data = await searchService.getNextData();
+
+    if (!data || (data.length == 0)) {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      return;
+    }
+      
+    if (searchService.page === 1) {
+      Notiflix.Notify.success(`Hooray! We found ${searchService.resultsQty} images.`);
+      if (useInfiniteScroll) {
+        observer.observe(refs.guardDiv);
       }
-    })
-    .catch(error => console.log(error))
+    }
+
+    refs.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(data));
+
+    refreshPage();
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    refs.btnSearch.disabled   = false;
+    refs.btnLoadMore.disabled = false;
+  }
 }
 
-function onLoadMoreBtn() {
-  page += 1
-  simpleLightBox.destroy()
-
-  fetchImages(query, page, perPage)
-    .then(({ data }) => {
-      renderGallery(data.hits)
-      simpleLightBox = new SimpleLightbox('.gallery a').refresh()
-
-      const totalPages = Math.ceil(data.totalHits / perPage)
-
-      if (page > totalPages) {
-        loadMoreBtn.classList.add('is-hidden')
-        alertEndOfSearch()
-      }
-    })
-    .catch(error => console.log(error))
+function createGalleryMarkup(data) {
+  return data.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${largeImageURL}">
+        <div class="gallery__thumb">
+          <img 
+            class="gallery__image"
+            src="${webformatURL}"
+            data-source="${largeImageURL}"
+            alt="${tags}"
+            loading="lazy" />
+        </div>    
+        <div class="gallery__info">
+          <p class="info__item">
+            <b>Likes</b><br>${likes}
+          </p>
+          <p class="info__item">
+            <b>Views</b><br>${views}
+          </p>
+          <p class="info__item">
+            <b>Comments</b><br>${comments}
+          </p>
+          <p class="info__item">
+            <b>Downloads</b><br>${downloads}
+          </p>
+        </div>
+      </a>
+    </li>
+  `).join('');
 }
 
-function alertImagesFound(data) {
-  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
-}
+function refreshPage() {
 
-function alertNoEmptySearch() {
-  Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.')
-}
+  lightbox.refresh();
 
-function alertNoImagesFound() {
-  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-}
+  if (searchService.isLastPage()) {
+    if (useInfiniteScroll) {
+      observer.unobserve(refs.guardDiv);
+    } else {
+      refs.btnLoadMore.classList.add('is-hidden');
+    }
+    refs.finishText.classList.remove('is-hidden');
+  } else {
+    if (!useInfiniteScroll) {
+      refs.btnLoadMore.classList.remove('is-hidden');
+    }
+    refs.finishText.classList.add('is-hidden');
+  }
+ 
+  if ((searchService.page > 1) && !useInfiniteScroll) {   
 
-function alertEndOfSearch() {
-  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+    const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: "smooth",
+    });
+  }
 }
